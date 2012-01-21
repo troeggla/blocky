1a09518774c329122ef5ce0cf83cc2f204cf0c93
Thomas R?ggla (PC4 - SuSE) <tom@lichtfragmente.com>
2012-01-21 14:50:00 +0100

    altered get_var to look up variables in parent scopes. if none is found, a new variable is allocated

--- src/blockScope.hpp
+++ src/blockScope.hpp
@@ -8,12 +8,17 @@
 #include "statement.hpp"
 
 class BlockScope {
+    BlockScope *parent;
     std::vector<Statement*> statements;
     std::map<std::string, double> variables;
 
 public:
+    BlockScope(BlockScope*);
+
     void add_var(std::string, double);
     void delete_var(std::string);
+
+    double find_var(std::string);
     double get_var(std::string);
 
     void add_statement(Statement*);
